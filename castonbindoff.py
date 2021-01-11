8703037c7c2edf3d3c727fa377fd723c2ab5fabf
kausalyamahadevan <kausalya.mahadevan@gmail.com>
2021-01-11 15:08:54 -0500

    switched to function

--- castonbindoff.py
+++ castonbindoff.py
@@ -1,12 +1,12 @@
 import knitout
 import numpy as np
 
-xsize = 5
+xsize = 15
 '''Functions to knit a section. Can be stacked.
    Should always end with the carriage and yarn feeder on the left
    However, "side" tells us what side the carriage (and yarn feeder) is on at the beginning'''
 
-def catchyarns(width,carriers):
+def catchyarns(k,width,carriers):
     k.rack(0)
     for c in carriers:
         for h in range(1,7):
@@ -24,7 +24,7 @@ def catchyarns(width,carriers):
                         k.tuck('-',('f',s),c)
             k.miss('+',('f',width),c) #moves carriers to the edge, maybe not necessary?
 
-def interlock(width,length,c,side):
+def interlock(k,width,length,c,side):
     k.rack(0)
     k.rollerAdvance(150)
     if side == 'r':
@@ -48,7 +48,7 @@ def interlock(width,length,c,side):
                 else:
                     k.knit('+',('b',s),c)
 
-def circular(width,length,c,side):
+def circular(k,width,length,c,side):
     k.rack(0)
     k.rollerAdvance(150)
     if side == 'r':
@@ -63,44 +63,34 @@ def circular(width,length,c,side):
             for s in range(1,width+1):
                 k.knit('+',('b',s),c)
 
-k = knitout.Writer('1 2 3 4 5 6')
-
-k.addHeader('Machine','kniterate')
-
-draw = '1'
-waste = '2'
-main = '3'
-k.ingripper(waste)
-k.ingripper(draw)
-k.ingripper(main)
-
 # cast on every needle
-catchyarns(xsize,['1','2','3'])
-#Move draw thread to the right side.
-for s in range(1,xsize+1):
-    k.knit('+',('f',s),draw)
+def caston(k,width,carriers):
+    #carriers is a list like ['1','2','3']
+    catchyarns(k,width,carriers)
+    draw,waste,main = carriers
+    #Move draw thread to the right side.
+    for s in range(1,width+1):
+        k.knit('+',('f',s),draw)
 
-k.rack(0.5)
-for s in range(1,xsize+1):
-    k.knit('+',('f',s),waste)
-    k.knit('+',('b',s),waste)
+    k.rack(0.5)
+    for s in range(1,width+1):
+        k.knit('+',('f',s),waste)
+        k.knit('+',('b',s),waste)
 
 
-#interlock / waste yarn
-interlock(xsize,16,waste,'r')
-#circular / waste Yarn
-circular(xsize,4,waste,'r')
+    #interlock / waste yarn
+    interlock(k,width,16,waste,'r')
+    #circular / waste Yarn
+    circular(k,width,4,waste,'r')
 
-for s in range(1,xsize+1):
-    k.drop(('b',s))
+    for s in range(1,width+1):
+        k.drop(('b',s))
 
-for s in range(xsize,0,-1):
-    k.knit('-',('f',s),draw)
+    for s in range(width,0,-1):
+        k.knit('-',('f',s),draw)
 
-#Case on main yarn!
-k.rack(0.5)
-for s in range(1,xsize+1):
-    k.knit('+',('f',s),main)
-    k.knit('+',('b',s),main)
-
-k.write('caston.k')
+    #Cast on main yarn!
+    k.rack(0.5)
+    for s in range(1,width+1):
+        k.knit('+',('f',s),main)
+        k.knit('+',('b',s),main)
---
+++ castonex.py
@@ -0,0 +1,14 @@
+from castonbindoff import *
+kwriter = knitout.Writer('1 2 3 4 5 6')
+
+kwriter.addHeader('Machine','kniterate')
+
+draw = '1'
+waste = '2'
+main = '3'
+kwriter.ingripper(waste)
+kwriter.ingripper(draw)
+kwriter.ingripper(main)
+
+caston(kwriter,xsize,[draw,waste,main])
+kwriter.write('caston.k')
