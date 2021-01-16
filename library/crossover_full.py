4496bbede6cb42d5546861430f37592043c9ac23
vsanchez01 <vs68@cornell.edu>
2021-01-15 21:23:47 -0500

    edited crossoverAll

--- library/crossover_full.py
+++ library/crossover_full.py
@@ -4,71 +4,69 @@ def crossoverFull(k,width,length,c,side):
 
     #account for starting position and add first row of knitting
     if side == 'l':
-        for w in range (1,width+1):
-            k.knit('+',('f',w-1),c)
+        for w in range (width):
+            k.knit('+',('f',w),c)
         start=2
-
-    else:
-        for w in range(width,0,-1):
-            k.knit('-',('f',w-1),c)
-
-        start=3
         length=length+1 #make sure we still get the full amount of passes desired
 
 
-    for z in range(start,length):
+    else:
+        for w in range(width-1,-1,-1):
+            k.knit('-',('f',w),c)
+
+        start=1
+
+
+    for z in range(start,length+1):
 
         if z%2==1:
 
             #knit all stitches
-            for w in range(1,width+1):
-                k.knit('+',('f',w-1),c)
+            for w in range(width):
+                k.knit('+',('f',w),c)
 
             #transfer all stitches to back
-            for w in range(1,width+1):
-                k.xfer(('f',w-1),('b',w-1))
+            for w in range(width):
+                k.xfer(('f',w),('b',w))
 
             #rack +1 and transfer every other stitch
             k.rack(1)
-            for w in range(1,width+1):
+            for w in range(1,width-1):
                 if w%2==1:
-                    k.xfer(('b',w-1),('f',w))
+                    k.xfer(('b',w),('f',w+1))
 
             #rack -1 and transfer
             k.rack(-1)
-            for w in range(1,width+1):
+            for w in range(1,width-1):
                 if w%2!=1:
-                    k.xfer(('b',w-1),('f',w-2))
+                    k.xfer(('b',w),('f',w+1))
 
 
         else:
-            for w in range(width,0,-1):
-                k.knit('-',('f',w-1),c)
+            for w in range(width-1,-1,-1):
+                k.knit('-',('f',w),c)
 
             #transfer all stitches to back
-            for w in range(width,0,-1):
-                k.xfer(('f',w-1),('b',w-1))
+            for w in range(width-2,0,-1):
+                k.xfer(('f',w),('b',w))
 
             #rack +1 and transfer every other stitch
             k.rack(1)
-            for w in range(width,0,-1):
+            for w in range(width-2,0,-1):
                 if w%2==1:
-                    k.xfer(('b',w-1),('f',w))
+                    k.xfer(('b',w),('f',w+1))
 
             #rack -1 and transfer
             k.rack(-1)
             for w in range(width,0,-1):
                 if w%2!=1:
-                    k.xfer(('b',w-1),('f',w-2))
+                    k.xfer(('b',w),('f',w-1))
 
     # make sure last line is knitting
     if length%2==1:
-        for w in range (1,width+1):
-            k.knit('+',('f',w-1),c)
+        for w in range (width):
+            k.knit('+',('f',w),c)
 
     else:
-        for w in range(width,0,-1):
-            k.knit('-',('f',w-1),c)
-
-
-k.write('acrossf.k')
+        for w in range(width-1-1,-1):
+            k.knit('-',('f',w),c)
