3bc2e7c995467c55a12f525a4c48a96619a40f6a
vsanchez01 <vs68@cornell.edu>
2021-02-14 12:40:39 -0500

    Added right to left bindoff

--- library/castonbindoff.py
+++ library/castonbindoff.py
@@ -120,8 +120,7 @@ def caston(k,width,carriers):
         k.knit('+',('b',s),carriers[2])
     circular(k,width,1,carriers[2],'r')
 
-''' Bind off function - note from now only goes from left to right
-and doesn't correct if  yarn in wrong place'''
+''' Bind off function"
 
 def bindoff(k, start,width,c,side='l',onfront=1):
 
@@ -135,45 +134,89 @@ def bindoff(k, start,width,c,side='l',onfront=1):
     k.rollerAdvance(50)
     k.addRollerAdvance(-50)
 
-    #first stitches start
-    k.tuck('-',('b',start-1),c)
-    k.knit('+',('b',start),c)
-    k.xfer(('b',start)),('f',start))
-    k.rack(-1)
-    k.xfer(('f',start)),('b',start+1))
-    k.rack(0)
-    k.knit('+',('b',start+1),c)
-
-    k.tuck('-',('b',start),c)
-    k.drop('b',0)
-    k.xfer(('b',start+1)),('f',start+1))
-    k.rack(-1)
-    k.xfer(('f',start+1)),('b',start+2))
-    k.rack(0)
-    k.addRollerAdvance(-50)
-    k.drop('b',start)
-    k.knit('+',('b',start+2),c)
-
-
-    for s in range(start+2,width):
-
-        k.tuck('-',('b',s-1),c)
-        k.xfer(('b',s),('f',s))
+    if side=='l':
+        #first stitches start
+        k.tuck('-',('b',start-1),c)
+        k.knit('+',('b',start),c)
+        k.xfer(('b',start)),('f',start))
         k.rack(-1)
-        k.xfer(('f',s),('b',s+1))
+        k.xfer(('f',start)),('b',start+1))
+        k.rack(0)
+        k.knit('+',('b',start+1),c)
+
+        k.tuck('-',('b',start),c)
+        k.drop('b',start-1)
+        k.xfer(('b',start+1)),('f',start+1))
+        k.rack(-1)
+        k.xfer(('f',start+1)),('b',start+2))
         k.rack(0)
         k.addRollerAdvance(-50)
-        k.drop('b',s-1)
-        k.knit('+',('b',s+1),c)
+        k.drop('b',start)
+        k.knit('+',('b',start+2),c)
 
-    #make the chain
-    k.rollerAdvance(200)
-    for m in range(8):
-        k.miss('+',('b',s+1),c)
-        k.knit('-',('b',s),c)
-        k.miss('-',('b',-1),c)
-        k.knit('+',('b',s),c)
 
-    #drop the last stitch
-    k.addRollerAdvance(200)
-    k.drop('b',s)
+        for s in range(start+2,width):
+
+            k.tuck('-',('b',s-1),c)
+            k.xfer(('b',s),('f',s))
+            k.rack(-1)
+            k.xfer(('f',s),('b',s+1))
+            k.rack(0)
+            k.addRollerAdvance(-50)
+            k.drop('b',s-1)
+            k.knit('+',('b',s+1),c)
+
+        #make the chain
+        k.rollerAdvance(200)
+        for m in range(8):
+            k.miss('+',('b',s+1),c)
+            k.knit('-',('b',s),c)
+            k.miss('-',('b',s-1),c)
+            k.knit('+',('b',s),c)
+
+        #drop the last stitch
+        k.addRollerAdvance(200)
+        k.drop('b',s)
+
+    else:
+        k.tuck('+',('b',width),c)
+        k.knit('-',('b',width-1),c)
+        k.xfer(('b',width-1)),('f',width-1))
+        k.rack(1)
+        k.xfer(('f',width-1)),('b',width-2))
+        k.rack(0)
+        k.knit('-',('b',width-2),c)
+
+        k.tuck('+',('b',width-1),c)
+        k.drop('b',width)
+        k.xfer(('b',width-2)),('f',width-2))
+        k.rack(1)
+        k.xfer(('f',width-2)),('b',start-3))
+        k.rack(0)
+        k.addRollerAdvance(-50)
+        k.drop('b',width-1)
+        k.knit('-',('b',knit-3),c)
+
+
+        for s in range(width-2,-1,-1):
+
+            k.tuck('+',('b',s+1),c)
+            k.xfer(('b',s),('f',s))
+            k.rack(1)
+            k.xfer(('f',s),('b',s-1))
+            k.rack(0)
+            k.addRollerAdvance(-50)
+            k.drop('b',s+1)
+            k.knit('-',('b',s-1),c)
+
+        #make the chain
+        k.rollerAdvance(200)
+        for m in range(8):
+            k.miss('-',('b',s-1),c)
+            k.knit('+',('b',s),c)
+            k.miss('+',('b',s+1),c)
+            k.knit('-',('b',s),c)
+
+        #drop the last stitch
+        k.addRollerAdvance(200)
+        k.drop('b',s)
