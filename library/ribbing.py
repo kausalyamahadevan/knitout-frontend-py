765a160adbc8f5affba877fabed3fc1acb18a6fe
kausalyamahadevan <kausalya.mahadevan@gmail.com>
2021-02-22 18:31:26 -0500

    comments

--- library/ribbing.py
+++ library/ribbing.py
@@ -3,8 +3,12 @@ import numpy as np
 FOR ALL ARRAYS: 1 : knit on BACK bed
                 0 : knit on FRONT bed
 '''
+
 '''
 Transfers stitches from cast on configuration (on all needles f and b) to ribbing
+k: knitout writer object
+ribarray: numpy array/list of 0s and 1s to define rib pattern
+repeats: integer, defines width of sample as a multiple of the ribarray length
 '''
 def xfertorib(k,ribarray,repeats):
     ribsize = len(ribarray)
@@ -19,6 +23,13 @@ def xfertorib(k,ribarray,repeats):
 '''
 knits as many rows as you like of a given rib knit pattern.
 Stitches must already be configured on the correct needles
+k: knitout writer object
+ribarray: numpy array/list of 0s and 1s to define rib pattern
+repeats: integer, defines width of sample as a multiple of the ribarray length
+length: number of rows knit
+c: string defining yarn carrier
+side: string defining which side the carrier starts on
+n0: integer defining '0' needle. Defaults to 0
 '''
 def ribKnit(k,ribarray,repeats,length,c,side='l',n0=0):
     ribsize = len(ribarray)
@@ -44,6 +55,10 @@ def ribKnit(k,ribarray,repeats,length,c,side='l',n0=0):
                     k.knit('-',('f',s+n0),c)
 
 
+''' This function ribknitrange is likely a duplicate of the above
+    for new scripts, use ribKnit.
+    Leaving this uncommented for now, will eventually be "depreciated" (HEHE)'''
+
 def ribKnitRange(k,ribarray,start,finish,length,c,side='l',n0=0):
     ribsize = len(ribarray)
     w  = ribsize*repeats
@@ -67,6 +82,14 @@ def ribKnitRange(k,ribarray,start,finish,length,c,side='l',n0=0):
                 else:
                     k.knit('-',('f',s+n0),c)
 
+''' must start in 1 x 1 configuration
+k: knitout writer object
+width: integer, sample width
+length: number of rows knit
+c: string defining yarn carrier
+side: string defining which side the carrier starts on
+n0: integer defining '0' needle. Defaults to 0
+'''
 def fishermansrib(k,width,length,c,side='l',n0=0):
     if side == 'r':
         start = 1
@@ -89,6 +112,8 @@ def fishermansrib(k,width,length,c,side='l',n0=0):
 '''
 Given two arrays of the same size, sets up for the
 rib pattern given in ribarray2
+ribarray: numpy array/list of 0s and 1s to define current needle configuration
+ribarray: numpy array/list of 0s and 1s to define future needle configuration
 '''
 
 def rib2ribXfer(k,ribarray1,ribarray2,repeats):
@@ -107,7 +132,13 @@ def rib2ribXfer(k,ribarray1,ribarray2,repeats):
     k.addRollerAdvance(300)
     k.rollerAdvance(400)
 
-''' Given an array and repeats, knits and purls'''
+''' Given an array and repeats, knits and purls
+k: knitout writer object
+array: numpy array of 0s and 1s to define rib pattern
+xrepeats: integer, defines width of sample as a multiple of the ribarray dimension
+xrepeats: integer, defines height of sample as a multiple of the ribarray dimension
+c: string defining yarn carrier
+side: string defining which side the carrier starts on'''
 
 def knitArray(k,array,xrepeats,yrepeats,c,side='l'):
     m, n = array.shape
